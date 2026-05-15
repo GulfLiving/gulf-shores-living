4b157b601d2a1930787be239e7b7f9fdd880802f
GulfLiving <tom@advancedconstruction.org>
2026-05-15 11:10:26 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -399,8 +399,8 @@ const gallery = [
             </div>
 
             <div className="grid gap-4 sm:grid-cols-2">
-              <div className="overflow-hidden rounded-3xl bg-white/5 ring-1 ring-white/10 sm:col-span-2"><img src="/images/gulf-shores-living/IMG_0308.jpg" alt="Private dock at Gulf Shores Living" className="h-64 w-full object-cover" /></div>
-              <div className="overflow-hidden rounded-3xl bg-white/5 ring-1 ring-white/10"><img src="/images/gulf-shores-living/IMG_0305.jpg" alt="Deck loungers overlooking the water" className="h-48 w-full object-cover" /></div>
+              <div className="overflow-hidden rounded-3xl bg-white/5 ring-1 ring-white/10 sm:col-span-2"><img src="/images/gulf-shores-living/IMG_0308.jpg" alt="Private dock at Gulf Shores Living" className="h-64 w-full object-cover object-center" /></div>
+              <div className="overflow-hidden rounded-3xl bg-white/5 ring-1 ring-white/10"><img src="/images/gulf-shores-living/Outdoor lounge area.jpg" alt="Deck loungers overlooking the water" className="h-48 w-full object-cover" /></div>
               <div className="overflow-hidden rounded-3xl bg-white/5 ring-1 ring-white/10"><img src="/images/gulf-shores-living/pier with ladder for swim.jpg" alt="Waterfront sunset view" className="h-48 w-full object-cover" /></div>
             </div>
           </div>
